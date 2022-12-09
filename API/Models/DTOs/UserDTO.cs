c20e84dcfc5ae285412f6705a56f1c9cfeb8a6ae
abdelmhe <92818538+abdelmhe@users.noreply.github.com>
2022-12-08 20:31:02 -0500

    Second Commit

---
+++ API/Models/DTOs/UserDTO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models.DTOs
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public List<string> ImagesUrls { get; set; }
+        
+    }
+}
