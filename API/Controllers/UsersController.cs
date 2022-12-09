f7fe41bd93621917bae08111eb701c82cdc180d2
abdelmhe <92818538+abdelmhe@users.noreply.github.com>
2022-12-08 22:22:41 -0500

    //POST /api/users/

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Models.Entities;
+using API.Models.Helpers;
+using API.Models.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -17,6 +20,9 @@ namespace API.Controllers
             this._context = context;
         }
 
+
+        
+
         /*
         POST /api/users/
         POST /api/users/{id}/image
@@ -26,6 +32,62 @@ namespace API.Controllers
         */
 
         //POST /api/users/
+
+          [HttpPost]
+        public async Task<IActionResult> AddUser([FromBody] User user)
+        {
+            bool isEmailExisting = UserHelper.IsEmailPresent(_context, user.Email);
+            ErrorR errorR = new ErrorR();
+
+           
+
+            if (String.IsNullOrEmpty(user.Name))
+            {
+                errorR.errors.Add(new Error()
+                {
+                    Status = "400",
+                    Title = "Name should not be empty",
+                    Detail = "Name should not be empty"
+                });
+
+                return BadRequest();
+            }
+            else if (String.IsNullOrEmpty(user.Email))
+            {
+                errorR.errors.Add(new Error()
+                {
+                    Status = "400",
+                    Title = "Email should not be empty",
+                    Detail = "Email should not be empty"
+                });
+            }
+            else if (isEmailExisting)
+            {
+                errorR.errors.Add(new Error()
+                {
+                    Status = "400",
+                    Title = "Email already exists",
+                    Detail = "This email already exists in the database"
+                });
+            }
+            else if (!UserHelper.IsEmailValid(user.Email))
+            {
+                errorR.errors.Add(new Error()
+                {
+                    Status = "400",
+                    Title = "Email is not valid",
+                    Detail = "This email is not valid"
+                });
+            }
+            else
+            {
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            return BadRequest(errorR);
+        }
         
 
 
